1339d0b1a4f91cb7d0743bf74e8d1e1ac954054e
dhakarabhishek <dhakarabhishek07@gmail.com>
2025-09-05 10:58:45 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -2,7 +2,7 @@ import requests
 url = "https://telegram.org/support"
 
 data = {
-    "problem": "This channel https://t.me/ is running illegal activities.\n"
+    "problem": "This channel https://t.me/astrology_premium_course is running illegal activities.\n"
     "It takes money from people to teach DDoS attacks on BGMI, hacking courses,\n"
     "and attacks on Indian government sites. It also performs DDoS attacks on Telegram VCs.\n"
     "Now, the channel owner has cleared history, so please review its past week's logs.",
@@ -17,4 +17,5 @@ if response.status_code == 200:
     print("Report submitted successfully!")
 else:
     print(f"Failed to submit report. Status code: {response.status_code}")
+
 # file owner @SOULCRACK
